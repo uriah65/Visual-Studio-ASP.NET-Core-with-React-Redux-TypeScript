ce68a93a1b01ba0fbab7cf8400626804f3dff010
Victor Cherniavsky <Victor.Cherniavsky@fsco.gov.on.ca>
2016-12-30 10:46:21 -0500

    .

--- src/CoreReactRedux/Startup.cs
+++ src/CoreReactRedux/Startup.cs
@@ -28,9 +28,11 @@ namespace CoreReactRedux
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Please navigate to Default.html.");
             });
         }
     }
